4f09f009f54c124d4404c40be33ec53d1d8677b9
VidTDM <realvidtdm@gmail.com>
2023-10-15 19:59:25 +0530

    add: open in new window option

--- src/generator.ts
+++ src/generator.ts
@@ -1,11 +1,23 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import { render } from "mustache";
-// TODO: ADD LINK AFTER PUBLISHING
+//! ADD LINK AFTER PUBLISHING
+// TODO: Add create in same directory configuration
+/*
+          "datapack-generator.createInThisDirectory": {
+            "type": [
+              "boolean"
+            ],
+            "default": false,
+            "description": "Tells whether to create the datapack in this directory or not."
+          }
+*/
 
 /* The above class is a TypeScript code that generates a Minecraft data pack with user-defined
 parameters. */
 export default class Generator {
+    private readonly configurations =
+        vscode.workspace.getConfiguration("datapack-generator");
     private readonly vsfs = vscode.workspace.fs;
     private readonly folder = vscode.workspace.workspaceFolders![0]?.uri;
     private readonly NAME = "Datapack";
@@ -13,21 +25,23 @@ export default class Generator {
     private readonly DESCRIPTION = "Generated using Datapack Generator";
     private readonly LASTEST_PACK_FORMAT = 15;
 
-    public init() { this.input() }
+    public init(): void {
+        this.input();
+    }
     /**
      * The below function takes user input from a VSCode input box and uses it to generate a name,
      * namespace, description, and pack format for further processing.
      */
-    private input() {
+    private input(): void {
         const input = vscode.window.showInputBox({
             value: ",,,",
-            prompt: "<name:string>,<namespace:string>,<description:string>,<pack_format:integer>\nLeave put two spaces ",
+            prompt: "<name:string>,<namespace:string>,<description:string>,<pack_format:integer>(separted by commas)",
         });
         input.then((val) => {
             if (!val) return;
 
             const options: Array<string> = [];
-            val.split(",").forEach(val => options.push(val));
+            val.split(",").forEach((val) => options.push(val));
 
             const name: string = options[0]
                 ? options[0].replace(/<|>|:|"|\/|\\|\||\?/gm, "-")
@@ -53,17 +67,28 @@ export default class Generator {
      * @param {number} pack_format - The `pack_format` parameter is a number that represents the format version of the pack. It is used to determine compatibility with different versions of Minecraft.
      */
     // prettier-ignore
-    private generate(name: string, namespace: string, description: string, pack_format: number) {
-        const pack_mcmetaText = fs.
-            readFileSync(`${__dirname}\\..\\src\\templates\\pack.mcmeta.mst`).toString();
-        const load_jsonText = fs.
-            readFileSync(`${__dirname}\\..\\src\\templates\\pack.mcmeta.mst`).toString();
-        const tick_jsonText = fs.
-            readFileSync(`${__dirname}\\..\\src\\templates\\pack.mcmeta.mst`).toString();
-        const pack_mcmeta = render(pack_mcmetaText, {
-            pack_format,
-            description,
-        });
+    private generate(name: string, namespace: string, description: string, pack_format: number): void {
+        const pack_mcmeta = render(
+            fs.
+                readFileSync(`${__dirname}\\..\\src\\templates\\pack.mcmeta.mst`)
+                .toString(),
+            {
+                pack_format,
+                description
+            }
+        );
+        const load_json = render(
+            fs.
+                readFileSync(`${__dirname}\\..\\src\\templates\\load.json.mst`)
+                .toString(),
+                { namespace }
+        );
+        const tick_json = render(
+                fs
+                    .readFileSync(`${__dirname}\\..\\src\\templates\\tick.json.mst`)
+                    .toString(),
+                { namespace }
+        );
 
         // Generate Main Folders
         this.vsfs.createDirectory(vscode.Uri.joinPath(this.folder, name));
@@ -77,11 +102,11 @@ export default class Generator {
         this.vsfs.createDirectory(vscode.Uri.joinPath(this.folder, `${name}/data/minecraft/tag/functions`));
         this.vsfs.writeFile(
             vscode.Uri.joinPath(this.folder, `${name}/data/minecraft/tag/functions/load.json`),
-            new TextEncoder().encode(load_jsonText)
+            new TextEncoder().encode(load_json)
         );
         this.vsfs.writeFile(
             vscode.Uri.joinPath(this.folder, `${name}/data/minecraft/tag/functions/tick.json`),
-            new TextEncoder().encode(tick_jsonText)
+            new TextEncoder().encode(tick_json)
         );
 
         // Generate Functions
@@ -97,15 +122,37 @@ export default class Generator {
 
         this.openNewWindow(vscode.Uri.joinPath(this.folder, name));
     }
-    private openNewWindow(path: vscode.Uri) {
-        vscode.window
-            .showInformationMessage('Open datapack in new window?', ...['Yes', 'No'])
-            .then(sel => {
-                if (sel === 'No') return;
-                vscode.commands.executeCommand('vscode.newWindow', { path: path.toString() });
-            });
+    /**
+     * The function `openNewWindow` checks the user's configuration and prompts them to open a new window or opens a new window directly based on the configuration.
+     * @param path - The `path` parameter is the path to the file or directory that you want to open in a new window. It is of type `vscode.Uri`.
+     */
+    private openNewWindow(path: vscode.Uri): void {
+        switch (this.configurations.openNewWindow) {
+            case null:
+                vscode.window
+                    .showInformationMessage(
+                        "Open datapack in new window?",
+                        ...["Yes", "No"]
+                    )
+                    .then((sel) => {
+                        if (sel === "No") return;
+                        vscode.commands.executeCommand(
+                            "vscode.openFolder",
+                            path,
+                            true
+                        );
+                    });
+                break;
+
+            case true:
+                vscode.commands.executeCommand("vscode.openFolder", path, true);
+                break;
+
+            case false:
+                break;
+        }
     }
-    public abort(err: unknown) {
+    public abort(err: any): void {
         throw new Error(`Extension action aborted due to ${err}`);
     }
 }
